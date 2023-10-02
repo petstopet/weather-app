35cf05e28d1f8e9c313460e1e5e8c69826628601
petstopet <144599590+petstopet@users.noreply.github.com>
2023-10-02 23:29:58 +0200

    Added weekly forecast
    Added weekly forecast

--- src/index.js
+++ src/index.js
@@ -27,6 +27,82 @@ let month = months[now.getMonth()];
 
 currentTime.innerHTML = `${day}, ${hours}:${minutes}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` 
+            <div class="col-2 days">
+              <img src="https://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png" alt="${
+          forecastDay.weather[0].description
+        }" width="60px" />
+              <div class="weather-forecast-temperatures">
+              <span class="temperature-max"> ${Math.round(
+                forecastDay.temp.max
+              )} </span>
+              /
+             <span class="temperature-min"> ${Math.round(
+               forecastDay.temp.min
+             )}  </span>
+              </div>
+            <div class="weather-forecast-date">
+            ${formatDay(forecastDay.dt)}
+            </div>
+           </div>  
+           `;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "7d478f69e1b2f5d563653f13f5f91d76";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayWeather(response) {
+  celsiusTemperature = response.data.main.temp;
+  let mainTemperature = document.querySelector("#mainTemperature");
+  let temperature = Math.round(response.data.main.temp);
+  mainTemperature.innerHTML = `${temperature}`;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
+}
+
+function search(city) {
+  let apiKey = "7d478f69e1b2f5d563653f13f5f91d76";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
 function newLocation(event) {
   event.preventDefault();
   let apiKey = "7d478f69e1b2f5d563653f13f5f91d76";
@@ -53,22 +129,6 @@ function showCelsius(event) {
 
 let celsiusTemperature = null;
 
-function displayWeather(response) {
-  celsiusTemperature = response.data.main.temp;
-  let mainTemperature = document.querySelector("#mainTemperature");
-  let temperature = Math.round(response.data.main.temp);
-  mainTemperature.innerHTML = `${temperature}`;
-  let windElement = document.querySelector("#wind");
-  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
-
-  let iconElement = document.querySelector("#icon");
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
-}
-
 let searchButton = document.querySelector("#searchButton");
 searchButton.addEventListener("click", newLocation);
 
@@ -77,3 +137,5 @@ fahrenheitLink.addEventListener("click", showFahrenheit);
 
 let celsiusLink = document.querySelector("#celsius");
 celsiusLink.addEventListener("click", showCelsius);
+
+search("Zagreb");
